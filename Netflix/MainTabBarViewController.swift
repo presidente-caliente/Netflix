a5b60c9ef31e45ca95b5cd6cd2c66170d9bfbde2
presidente-caliente <marinedevops@gmail.com>
2022-05-15 01:52:00 -0600

    instantiate controllers in main tab bar class

--- Netflix/MainTabBarViewController.swift
+++ Netflix/MainTabBarViewController.swift
@@ -16,6 +16,12 @@ class MainTabBarViewController: UITabBarController {
         // define the views background color
         view.backgroundColor = .systemYellow
         
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        
+        setViewControllers([vc1,vc2,vc3,vc4], animated: true)
     }
 
 
