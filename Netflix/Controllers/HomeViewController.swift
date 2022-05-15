e6571517f3c2084b8cf3eb558365a7d0b0bd5987
presidente-caliente <marinedevops@gmail.com>
2022-05-15 02:21:19 -0600

    instantiate a table cell view method the home view controller

--- Netflix/Controllers/HomeViewController.swift
+++ Netflix/Controllers/HomeViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let homeFeedTable: UITableView = {
+        
+        // instantiate single column row
+        let table = UITableView()
+        
+        // register function for use
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
+        
+        // add a subview
+        view.addSubview(homeFeedTable)
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // define homeFeedTable object bounds within view
+        homeFeedTable.frame = view.bounds
     }
 
 }
