5dbca01fa79aceab9064d44eea9ad21aea0da25c
presidente-caliente <marinedevops@gmail.com>
2022-05-18 23:19:36 -0600

    add a fade to header view

--- Netflix/Views/HeroHeaderUIView.swift
+++ Netflix/Views/HeroHeaderUIView.swift
@@ -28,13 +28,36 @@ class HeroHeaderUIView: UIView {
         return imageView
     }()
     
+    // add gradient to hero header view image
+    func addGradient(){
+        
+        let gradientLayer = CAGradientLayer()
+        
+        // create colors array
+        gradientLayer.colors = [
+            UIColor.clear.cgColor, // let the image show through gradiant
+            UIColor.systemBackground.cgColor // fade into background color
+        ]
+        
+        // define the subview properites in window
+        gradientLayer.frame = bounds
+        
+        // add the gradiant subview as a sublayer to the core layer
+        layer.addSublayer(gradientLayer)
+        
+    }
+    
     // create view: any subclass inherits
     override init(frame: CGRect) {
         // documentation defines when to call super
         super .init(frame: frame)
         
         addSubview(heroImageView)
+        
+        addGradient()
     }
+    
+    
     
     // define custom subview layouts
     /* suggestion: use setNeedsLayout() instead */
